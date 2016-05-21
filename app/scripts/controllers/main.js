4a4e58cdf07085c564502c8579d507f58debb019
huj <hubert.jacquin@awtce.be>
2016-05-21 17:30:25 +0200

    create project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCrossfilterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularCrossfilterApp
+ */
+angular.module('angularCrossfilterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
